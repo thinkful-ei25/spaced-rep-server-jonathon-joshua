6c80b2e34024b4f58d77d31f885a40a79e317f5d
Reldalk <Reldalk@gmail.com>
2019-01-09 17:14:40 -0500

    added new head get method

--- routes/questions.js
+++ routes/questions.js
@@ -3,11 +3,27 @@ const express = require('express');
 const router = express.Router();
 const userQuestion = require('../schema/user'); 
 
+router.get('/head/:id', (req, res) => {
+  let id = req.params.id;
+  console.log(id);
+  
+  userQuestion.findById(id, {'_id':0})
+    .select('head')
+    .then(questions => {
+      res.json({
+        questions
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
 
 router.get('/:id', (req, res) => {
   let value = req.query.category;
   let id = req.params.id;
-  console.log(value);
+
+  console.log(id);
   
   userQuestion.findById(id, {'_id':0})
     .select(value)
